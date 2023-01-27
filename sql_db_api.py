a898c61cdb49af4f9048f62a0e9d03166a6afb5b
ahsant161 <121575052+ahsant161@users.noreply.github.com>
2023-01-27 13:14:17 -0600

    Add files via upload
    Connecting to SQL database in IBM Cloud, issuing SQL statements, all via Python

---
+++ sql_db_api.py
@@ -0,0 +1,39 @@
+import ibm_db
+
+dsn_hostname = "b70af05b-76e4-4bca-a1f5-23dbb4c6a74e.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud" # e.g.: "54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "rct68126"        # e.g. "abc12345"
+dsn_pwd = "cLBv1lMiUhz4ZxEL"      # e.g. "7dBZ3wWt9XN6$o0J"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"            # e.g. "BLUDB"
+dsn_port = "32716"                # e.g. "32733"
+dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+dsn_security = "SSL"
+
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
+
+print(dsn)
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+except:
+    print ("Unable to connect: ", ibm_db.conn_errormsg() )
+
+
+selectQuery = "select * from INSTRUCTOR"
+
+selectStmt = ibm_db.exec_immediate(conn, selectQuery)
+
+while ibm_db.fetch_row(selectStmt) != False:
+    result = ibm_db.result(selectStmt, "FNAME")
+    print(result)
